3991c87eabf625d97aff764b3bd93383ba05d56b
prathamo28 <123449708+prathamo28@users.noreply.github.com>
2023-04-26 16:41:10 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,13 +10,13 @@ variable "instance_data" {
     {
       instance_id      = "i-0fae66b6348bdb0a2"
       ami_id           = "ami-02396cdd13e9a1257"
-      instance_type    = "t2.micro"
+      instance_type    = "t2.small"
       name             = "my_ec2_new"
     },
     {
       instance_id      = "i-0410e332568b4595d"
       ami_id           = "ami-02396cdd13e9a1257"
-      instance_type    = "t3.small"
+      instance_type    = "t3.micro"
       name             = "my_ec2_new2"
     }
   ] 
