c3b9382f6eb34458de71bee34ab837eb0655ddae
prathamo28 <prathamesh.mokal28@gmail.com>
2023-10-18 20:04:01 +0530

    policy

--- main.tf
+++ main.tf
@@ -1,76 +1,113 @@
-#Creating KMS key 
-resource "aws_kms_key" "alt_backup_day_key" {
-  description = "Alt-Backup-key-day-vault"
-}
-
-#Creating AWS Vault policy 
-data "aws_iam_policy_document" "alt_backup_vault_policy" {
+data "aws_iam_policy_document" "altrata_test_policy" {
   statement {
     sid       = ""
     effect    = "Allow"
     resources = ["*"]
-    actions   = ["backup:CopyIntoBackupVault",
-                 "backup:CopyFromBackupVault",
-                 "backup:CopyIntoBackupVault",
-                 "backup:CreateReportPlan",
-                 "backup:DescribeBackupJob",
-                 "backup:DescribeBackupVault",
-                 "backup:DescribeCopyJob",
-                 "backup:DescribeFramework",
-                 "backup:DescribeProtectedResource",
-                 "backup:DescribeRecoveryPoint",
-                 "backup:DescribeReportJob",
-                 "backup:DescribeReportPlan",
-                 "backup:DescribeRestoreJob",
-                 "backup:ListBackupJobs",
-                 "backup:ListBackupPlans",
-                 "backup:ListBackupVaults",
-                 "backup:ListCopyJobs",
-                 "backup:StartBackupJob",
-                 "backup:StartCopyJob",
-                 "backup:StartReportJob",
-                 "backup:StartRestoreJob",
-                 "backup:StopBackupJob"]
+
+    actions = [
+      "kms:DescribeKey",
+      "kms:Decrypt",
+      "kms:Encrypt",
+      "kms:GenerateDataKey",
+      "kms:ReEncrypt*",
+    ]
 
     condition {
-      test     = "ForAnyValue:StringLike"
-      variable = "aws:PrincipalOrgPaths"
-      values   = ["o-1llr6jd9b2/r-[RootID]/ou-[OU]/*"]
+      test     = "StringLike"
+      variable = "kms:ViaService"
+      values   = ["s3.*.amazonaws.com"]
     }
+  }
+
+  statement {
+    sid    = ""
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:s3:::*",
+      "arn:aws:s3:::*/*",
+    ]
+
+    actions = [
+      "s3:GetInventoryConfiguration",
+      "s3:PutInventoryConfiguration",
+      "s3:ListBucketVersions",
+      "s3:ListBucket",
+      "s3:GetBucketNotification",
+      "s3:PutBucketNotification",
+      "s3:GetBucketLocation",
+      "s3:GetBucketTagging",
+      "s3:GetBucketAcl",
+      "s3:CreateBucket",
+      "s3:GetBucketVersioning",
+      "s3:PutBucketVersioning",
+      "s3:GetObjectAcl",
+      "s3:GetObject",
+      "s3:GetObjectVersionTagging",
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectTagging",
+      "s3:GetObjectVersion",
+      "s3:DeleteObject",
+      "s3:PutObjectVersionAcl",
+      "s3:GetObjectVersionAcl",
+      "s3:PutObjectTagging",
+      "s3:PutObjectAcl",
+      "s3:PutObject",
+      "s3:ListMultipartUploadParts",
+    ]
+  }
+
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["s3:ListAllMyBuckets"]
+  }
+
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["arn:aws:events:*:*:rule/AwsBackupManagedRule*"]
+
+    actions = [
+      "events:DescribeRule",
+      "events:EnableRule",
+      "events:PutRule",
+      "events:DeleteRule",
+      "events:PutTargets",
+      "events:RemoveTargets",
+      "events:ListTargetsByRule",
+      "events:DisableRule",
+      "cloudwatch:GetMetricData",
+      "events:ListRules",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "alt_new_policy" {
+  name = "alt-get-policy"
+  policy = data.aws_iam_policy_document.alt_kms_policy.json
+}
+
+data "aws_iam_policy_document" "alt_test_role" {
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
 
     principals {
-      type        = "*"
-      identifiers = ["*"]
+      type        = "Service"
+      identifiers = ["backup.amazonaws.com"]
     }
   }
 }
 
-resource "aws_backup_vault" "alt_backup_day_vault" {
-  name        = "alt-vault-twenty-day-retention-v1"
-  kms_key_arn = aws_kms_key.alt_backup_day_key.arn
+resource "aws_iam_role" "alt_assume_role" {
+  name = "alt-assume-role"
+  assume_role_policy = data.aws_iam_policy_document.alt_test_role.json
 }
 
-resource "aws_backup_vault_policy" "alt_backup_day_vault_policy" {
-  backup_vault_name = aws_backup_vault.alt_backup_day_vault.name
-  policy            = data.aws_iam_policy_document.alt_backup_vault_policy.json
+resource "aws_iam_role_policy_attachment" "alt_ops_backup_role_attachment" {
+  policy_arn = aws_iam_policy.alt_new_policy.arn
+  role       = aws_iam_role.alt_assume_role.name
 }
-
-resource "aws_backup_plan" "alt_backup_day_plan" {
-  name = "alt-backup-day-plan"
-  rule {
-    rule_name = "alt-vault-twenty-day-plan"
-    target_vault_name = aws_backup_vault.alt_backup_day_vault.name
-    schedule = "cron(30 5 * * ? *)"
-    lifecycle {
-      delete_after = 2
-    }
-  }
-  advanced_backup_setting {
-    backup_options = {
-        WindowsVSS = "enabled"
-    }
-    resource_type = "EC2"
-  }
-}
-
-
