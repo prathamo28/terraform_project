8dea95ff9ea0988c14539fd862597e5b843139b1
prathamo28 <prathamesh.mokal28@gmail.com>
2023-10-19 15:42:14 +0530

    s3 bucket

--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
-variable "Account_id" {
-  default = "957840177530"
+variable "bucket_names" {
+  description = "List of S3 bucket names"
+  type        = list(string)
+  default     = ["alt-s3-database-backup-test-v1", "alt-s3-database-backup-prod-v1", "alt-s3-app-backup-test-v1", "alt-s3-app-backup-prod-v1"]
 }
