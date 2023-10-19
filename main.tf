ab4f9ae329c0640a01757f856381fcf6935609c6
prathamo28 <prathamesh.mokal28@gmail.com>
2023-10-19 15:43:34 +0530

    s3

--- main.tf
+++ main.tf
@@ -2,8 +2,3 @@ resource "aws_s3_bucket" "alt_s3_database_backup" {
   for_each = { for idx, name in var.bucket_names : idx => name }
   bucket   = each.value
 }
-
-resource "aws_s3_bucket" "alt_s3_database_backup" {
-  for_each = { for idx, name in var.bucket_names : idx => name }
-  bucket   = each.value
-}
