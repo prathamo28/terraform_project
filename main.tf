256848154fa0c98f009541bb95a5400b2bcea19d
prathamo28 <prathamesh.mokal28@gmail.com>
2023-10-26 17:54:58 +0530

    role name

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 resource "aws_iam_role" "alt_vault_role" {
-  name               = "example"
+  name               = "vault-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
