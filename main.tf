8fadec155da0bdc4189f3a0e960dc5b22f5205ca
prathamo28 <prathamesh.mokal28@gmail.com>
2023-10-26 17:31:18 +0530

    plan

--- main.tf
+++ main.tf
@@ -4,28 +4,27 @@ resource "aws_kms_key" "backup_key" {
   key_usage = "ENCRYPT_DECRYPT"
 }
 
-
 # Vault for thirty-day retention
 resource "aws_backup_vault" "thirty_day_retention_vault" {
   name                 = "alt-vault-thirty-day-retention-v1"
   kms_key_arn          = aws_kms_key.backup_key.arn
 }
-resource "aws_backup_vault_lock_configuration" "alt_backup_thirty_day_lock" {
-  backup_vault_name   = "alt-vault-thirty-day-retention-v1"
-  max_retention_days  = 7
-  min_retention_days  = 30
+
+resource "aws_backup_plan" "thirty_day_retention_plan" {
+  name = "example_backup_plan"
+  rule {
+    rule_name = "example_backup_rule"
+    target_vault_name = aws_backup_vault.thirty_day_retention_vault.name
+    schedule = "cron(0 5-6 * * *)"
+    
+    lifecycle {
+      delete_after = 2
+    }
+  }
 }
 
 
-resource "aws_backup_vault" "six_month_retention_vault" {
-  name                 = "alt-vault-six-month-retention-v1"
-  kms_key_arn          = aws_kms_key.backup_key.arn
-}
-resource "aws_backup_vault_lock_configuration" "alt_backup_six_month_lock" {
-  backup_vault_name   = "alt-vault-six-month-retention-v1"
-  max_retention_days  = 90
-  min_retention_days  = 180
-}
+
 
 
 
