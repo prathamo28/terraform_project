d0aee6eaf3cc38170daab95a9eb96ebb739733fa
prathamo28 <prathamesh.mokal28@gmail.com>
2023-10-27 16:37:56 +0530

    last test

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "aws_kms_key" "alt_vault_thirty_day_key" {
   key_usage   = "ENCRYPT_DECRYPT"
 }
 resource "aws_kms_alias" "alt_vault_thirty_day" {
-  for_each      = aws_kms_key.alt_vault_thirty_day_key
+  for_each    = var.regions
   name          = "alias/alt-vault-day-${each.key}"
-  target_key_id = aws_kms_key.alt_vault_thirty_day_key.key_id
+  target_key_id = aws_kms_key.alt_vault_thirty_day_key[each.key].key_id
 }
 
 resource "aws_kms_key" "alt_vault_six_month_key" {
@@ -17,32 +17,32 @@ resource "aws_kms_key" "alt_vault_six_month_key" {
   key_usage   = "ENCRYPT_DECRYPT"
 }
 resource "aws_kms_alias" "alt_vault_six_month" {
-  for_each      = aws_kms_key.alt_vault_six_month_key
+  for_each    = var.regions
   name          = "alias/alt-vault-month-${each.key}"
-  target_key_id = aws_kms_key.alt_vault_six_month_key.key_id
+  target_key_id = aws_kms_key.alt_vault_six_month_key[each.key].key_id
 }
 
 # Create aws backup vault in alt-secure account 
 resource "aws_backup_vault" "thirty_day_retention_vault" {
-  for_each    = aws_kms_key.alt_vault_thirty_day_key
-  name        = "alt-vault-thirty-day-retention-v1-${var.regions[count.index]}"
-  kms_key_arn = aws_kms_key.alt_vault_thirty_day_key.arn
+  for_each    = var.regions
+  name        = "alt-vault-thirty-day-retention-v1-${each.value}"
+  kms_key_arn = aws_kms_key.alt_vault_thirty_day_key[each.key].arn
 }
 resource "aws_backup_vault_lock_configuration" "alt_backup_day_lock" {
-  for_each            = aws_kms_key.alt_vault_thirty_day_key
-  backup_vault_name   = "alt-vault-twenty-day-retention-v1-${var.regions[count.index]}"
+  for_each    = var.regions
+  backup_vault_name   = "alt-vault-twenty-day-retention-v1-${each.value}"
   min_retention_days  = 7
   max_retention_days  = 30
 }
 
 resource "aws_backup_vault" "six_month_retention_vault" {
-  for_each    = aws_kms_key.alt_vault_six_month_key
-  name        = "alt-vault-six-month-retention-v1-${var.regions[count.index]}"
-  kms_key_arn = aws_kms_key.alt_vault_six_month_key.arn
+  for_each    = var.regions
+  name        = "alt-vault-six-month-retention-v1-${each.value}"
+  kms_key_arn = aws_kms_key.alt_vault_six_month_key[each.key].arn
 }
 resource "aws_backup_vault_lock_configuration" "alt_backup_month_lock" {
-  for_each            = aws_kms_key.alt_vault_six_month_key
-  backup_vault_name   = "alt-vault-six-month-retention-v1-${var.regions[count.index]}"
+  for_each    = var.regions
+  backup_vault_name   = "alt-vault-six-month-retention-v1-${each.value}"
   min_retention_days  = 90
   max_retention_days  = 180
 }
