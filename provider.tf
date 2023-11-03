56bc87a0c4a1557e9402e83a63ac6b75b6d03102
prathamo28 <prathamesh.mokal28@gmail.com>
2023-11-03 21:18:50 +0530

    provider

--- provider.tf
+++ provider.tf
@@ -10,29 +10,8 @@ terraform {
 # Configure the AWS Provider
 
 provider "aws" {
-  alias  = "us-east-1"
-  region = var.regions[0]
+  region = "us-east-1"
   access_key = "AKIA5TFR6JLPSZZ6ZVHQ"
   secret_key = "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"
 }
 
-provider "aws" {
-  alias  = "us-east-2"
-  region = var.regions[1]
-  access_key = "AKIA5TFR6JLPSZZ6ZVHQ"
-  secret_key = "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"
-}
-
-provider "aws" {
-  alias  = "ap-southeast-1"
-  region = var.regions[2]
-  access_key = "AKIA5TFR6JLPSZZ6ZVHQ"
-  secret_key = "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"
-}
-
-provider "aws" {
-  alias  = "eu-west-1"
-  region = var.regions[3]
-  access_key = "AKIA5TFR6JLPSZZ6ZVHQ"
-  secret_key = "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"
-}
